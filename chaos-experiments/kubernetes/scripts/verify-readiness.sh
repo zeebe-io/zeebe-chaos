c1311f16c7772f124377c4f61b342b15565198a8
Christopher Zell <zelldon91@googlemail.com>
2020-03-28 21:56:37 +0100

    chore(chaos/k8s): add namespace to ready script

--- chaos-experiments/kubernetes/scripts/verify-readiness.sh
+++ chaos-experiments/kubernetes/scripts/verify-readiness.sh
@@ -3,4 +3,4 @@ set -exo pipefail
 
 namespace=$(kubectl config view --minify --output 'jsonpath={..namespace}')
 # verify that everything is running
-kubectl wait --for=condition=Ready pod -l app=$namespace-zeebe --timeout=900s
+kubectl wait --for=condition=Ready pod -l app=$namespace-zeebe --timeout=900s -n $namespace
