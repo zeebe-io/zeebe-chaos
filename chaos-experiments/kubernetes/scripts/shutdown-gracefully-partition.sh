63c481c0c7dd7026f03be4e51d61a918613b0140
Christopher Zell <zelldon91@googlemail.com>
2020-10-21 15:34:54 +0200

    chore(chaos): terminate followers
     Adds new experiment which terminates the follower instead of shutdown the pod gracefully. Improves and fixes issues in the scripts, add new parameter to specify partition

---
+++ chaos-experiments/kubernetes/scripts/shutdown-gracefully-partition.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -exuo pipefail
+
+scriptPath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+source "$scriptPath/utils.sh"
+
+namespace=$(getNamespace)
+pod=$(getBroker)
+gateway=$(getGateway)
+
+state=$1
+partition=${2:-3}
+
+# To print the topology in the journal
+topology=$(kubectl exec "$gateway" -n "$namespace" -- zbctl status --insecure)
+
+# For cluster size 3 and replication factor 3
+# we know the following partition matrix
+# partition \ node  0    1     2
+#     1             L    F     F
+#     2             F    L     F
+#     3             F    F     L
+#    etc.
+# This means broker 1, 2 or 3 participates on partition 3
+
+
+index=$(($(echo "$topology" \
+  | grep "Partition $partition" \
+  | grep -n "$state" -m 1 \
+  | sed 's/\([0-9]*\).*/\1/') - 1))
+
+pod=$(echo "$pod" | sed 's/\(.*\)\([0-9]\)$/\1/')
+pod="$pod$index"
+
+echo "$pod" will be stopped
+
+kubectl delete pod "$pod" -n "$namespace" 
