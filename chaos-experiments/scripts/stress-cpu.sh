c43b24aff77ca5ffc0a2e43090d590848ef816e8
Christopher Zell <zelldon91@googlemail.com>
2020-11-17 14:08:20 +0100

    chore(chaos): rework stress script

---
+++ chaos-experiments/scripts/stress-cpu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -exuo pipefail
+
+target=${1:-"broker"}
+
+source utils.sh
+
+namespace=$(getNamespace)
+targetPod=$(getBroker 0)
+if [ "$target" == "gateway" ];
+then
+  targetPod=$(getGateway)
+fi
+
+# we need to update the system before installing new packages
+kubectl exec -n "$namespace" "$targetPod" -- apt update
+
+# we install stress and procps to kill it later with pgrep
+kubectl exec -n "$namespace" "$targetPod" -- apt install -y stress procps
+
+# stress cpu in background
+kubectl exec -n "$namespace" "$targetPod" -- stress --cpu 256 --vm 32 --vm-bytes 4M --timeout 30
+
