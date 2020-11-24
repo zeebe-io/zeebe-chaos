47aa4de7c5ab6583b9065560ce15b6979908b73d
Christopher Zell <zelldon91@googlemail.com>
2020-11-24 14:14:00 +0100

    chore(bpmn): add new model and deploy all models at once

--- chaos-experiments/scripts/deploy-model.sh
+++ chaos-experiments/scripts/deploy-model.sh
@@ -8,14 +8,13 @@ source utils.sh
 namespace=$(getNamespace)
 pod=$(getGateway)
 
-processFileName=one_task.bpmn
-bpmnPath="$scriptPath/../bpmn/$processFileName"
+bpmnPath="$scriptPath/../bpmn/"
 
 # we put both together in one function to retry both, because it might be that pod has been restarted
 # then the model is not longer on the node, which cause endless retries of deployments
 function deployModel() {
-  kubectl cp "$bpmnPath" "$pod:/tmp/$processFileName" -n "$namespace"
-  kubectl exec "$pod" -n "$namespace" -- zbctl deploy "/tmp/$processFileName" --insecure
+  kubectl cp "$bpmnPath" "$pod:/tmp/" -n "$namespace"
+  kubectl exec "$pod" -n "$namespace" -- sh -c "zbctl deploy /tmp/bpmn/*.bpmn --insecure"
 }
 
 retryUntilSuccess deployModel
