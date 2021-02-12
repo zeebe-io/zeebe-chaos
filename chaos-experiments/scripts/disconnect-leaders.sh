0de28e4e984de0a83af6f4f2bbd3034d77023749
Christopher Zell <zelldon91@googlemail.com>
2021-02-12 12:09:31 +0100

    chore(chaos): scripts for disconnect/connect leaderS

---
+++ chaos-experiments/scripts/disconnect-leaders.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -exuo pipefail
+
+source utils.sh
+
+partition=1
+namespace=$(getNamespace)
+gateway=$(getGateway)
+
+# determine leader for partition one
+index=$(getIndexOfPodForPartitionInState "$partition" "LEADER")
+leader=$(getBroker "$index")
+leaderIp=$(kubectl get pod "$leader" -n "$namespace" --template="{{.status.podIP}}")
+
+# determine leader for partition two
+
+index=$(getIndexOfPodForPartitionInState "3" "FOLLOWER")
+leaderTwo=$(getBroker "$index")
+leaderTwoIp=$(kubectl get pod "$leaderTwo" -n "$namespace" --template="{{.status.podIP}}")
+
+# To print the topology in the journal
+retryUntilSuccess kubectl exec "$gateway" -n "$namespace" -- zbctl status --insecure
+
+# we put all into one function because we need to make sure that even after preemption the 
+# dependency is installed
+function disconnect() {
+ toChangedPod="$1"
+ targetIp="$2"
+
+ # update to have access to ip
+ kubectl exec -n "$namespace" "$toChangedPod" -- apt update
+ kubectl exec -n "$namespace" "$toChangedPod" -- apt install -y iproute2
+ kubectl exec "$toChangedPod" -n "$namespace" -- ip route add unreachable "$targetIp"
+
+}
+
+retryUntilSuccess disconnect "$leader" "$leaderTwoIp"
+retryUntilSuccess disconnect "$leaderTwo" "$leaderIp" 
