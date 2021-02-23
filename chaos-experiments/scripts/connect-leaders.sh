c2e77dbd7ea59a62ecba4305b184a59d7e0f42d7
Christopher Zell <zelldon91@googlemail.com>
2021-02-23 14:11:38 +0100

    chore(scripts): improve dis/connect scripts for different leaders

--- chaos-experiments/scripts/connect-leaders.sh
+++ chaos-experiments/scripts/connect-leaders.sh
@@ -12,11 +12,26 @@ index=$(getIndexOfPodForPartitionInState "$partition" "LEADER")
 leader=$(getBroker "$index")
 leaderIp=$(kubectl get pod "$leader" -n "$namespace" --template="{{.status.podIP}}")
 
-# determine leader for partition two
-
-index=$(getIndexOfPodForPartitionInState "3" "FOLLOWER")
+# determine leader for partition three
+index=$(getIndexOfPodForPartitionInState "3" "LEADER")
 leaderTwo=$(getBroker "$index")
 leaderTwoIp=$(kubectl get pod "$leaderTwo" -n "$namespace" --template="{{.status.podIP}}")
+
+if [ "$leaderTwo" == "$leader" ]
+then
+  # determine leader for partition two
+  index=$(getIndexOfPodForPartitionInState "2" "LEADER")
+  leaderTwo=$(getBroker "$index")
+  leaderTwoIp=$(kubectl get pod "$leaderTwo" -n "$namespace" --template="{{.status.podIP}}")
+
+  if [ "$leaderTwo" == "$leader" ]
+  then
+    # We could try to kill the pod and hope that he is not able to become leader again,
+    # but there is a high chance that it is able to do so after restart. It can make our test fragile,
+    # especially if we want to connect again, which is the reason why we do nothing in that case.
+    exit
+  fi
+fi
 
 # To print the topology in the journal
 retryUntilSuccess kubectl exec "$gateway" -n "$namespace" -- zbctl status --insecure
