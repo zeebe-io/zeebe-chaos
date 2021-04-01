04a70e70f951874b55df792ec0055752ec5986c9
Christopher Zell <zelldon91@googlemail.com>
2021-04-01 12:01:02 +0200

    run instance with cluster plan type

--- chaos-model/runInstance.sh
+++ chaos-model/runInstance.sh
@@ -2,4 +2,6 @@
 
 # we need to replace steady-state-hypothesis because zeebe doesn't support variables names with dashes
 # https://github.com/camunda-cloud/zeebe/issues/6717
-zbctl --insecure create instance chaosExperiment --variables "$(sed 's/steady-state-hypothesis/steadyState/g' experiment.json)"
+#zbctl --insecure create instance chaosExperiment --variables "$(sed 's/steady-state-hypothesis/steadyState/g' experiment.json)"
+
+zbctl --insecure create instance chaosToolkit --variables '{"clusterPlan":"production-s"}'
