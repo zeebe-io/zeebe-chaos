d895f389b3821b0684090a80737e8d1ad91da001
Christopher Zell <zelldon91@googlemail.com>
2021-04-01 10:45:38 +0200

    first increment of chaos worker

--- chaos-model/chaos-worker/src/main/kotlin/main.kt
+++ chaos-model/chaos-worker/src/main/kotlin/main.kt
@@ -1,3 +1,44 @@
+import io.zeebe.client.ZeebeClient
+import io.zeebe.client.api.response.ActivatedJob
+import io.zeebe.client.api.worker.JobClient
+import java.io.File
+import java.util.concurrent.CountDownLatch
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    val latch = CountDownLatch(1)
+    val zeebeClient = ZeebeClient.newClientBuilder().usePlaintext().build()
+    (zeebeClient.newWorker().jobType("verify-readiness.sh").handler(::handler).open()).use {
+     latch.await()
+    }
+}
+
+fun handler(client: JobClient, activatedjob: ActivatedJob) {
+    println(activatedjob.variablesAsMap)
+    val provider = activatedjob.variablesAsMap["provider"]!! as Map<String, Any>
+
+    val args = provider["arguments"]
+    args?.let {
+        println("Arguments: $it")
+    }
+
+
+    val command = provider["path"]!!.toString()
+    val scriptPath = File("../../chaos-experiments/scripts/")
+    println("Path: ${scriptPath.absolutePath}")
+
+    val processBuilder = ProcessBuilder("$scriptPath/$command")
+        .inheritIO()
+        .directory(scriptPath)
+    processBuilder.environment().put("CHAOS_SETUP", "helm");
+    val process = processBuilder.start()
+    val exitValue = process.waitFor()
+
+    if (exitValue == 0)
+    {
+        client.newCompleteCommand(activatedjob.key).send()
+    }
+    else {
+        client.newFailCommand(activatedjob.key).retries(0).send();
+    }
 }
