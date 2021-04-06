9ed0f3ff29136a1763930aca5ef6c064138d1bd6
Christopher Zell <zelldon91@googlemail.com>
2021-04-06 20:12:59 +0200

    fix(chaos): fix corrupt snapshot script
     the snapshot names have been changed

--- chaos-experiments/scripts/corruptSnapshot.sh
+++ chaos-experiments/scripts/corruptSnapshot.sh
@@ -5,7 +5,7 @@ partition=$1
 
 # Find the latest snapshot directory
 partitionDir="data/raft-partition/partitions/$partition"
-snapshotDir=( "$partitionDir"/snapshots/*-*-* )
+snapshotDir=( "$partitionDir"/snapshots/* )
 
 # Remove a random *.sst file
 fileName=$(find "${snapshotDir[0]}" -name "*.sst" -print -quit)
