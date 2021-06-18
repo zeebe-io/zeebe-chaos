be705b1e64f61123973222240d1bba98f1eb1323
Christopher Zell <zelldon91@googlemail.com>
2021-06-18 08:52:42 +0200

    fix: make deployment distribution reliable
    Only run experiment for Production L clusters.
    Disconnect partition 1 and 4, such that the leaders are unrelated to
    each other and not trigger an election during network partition.
    
    The network partition is only in one direction, from Leader of partition
    one to the other leader, such that deployment distribution fails but
    nothing else. This was necessary since we use embedded gateways in the
    Camunda Cloud setup.
    
    Distribution:
    P\N|	N 0|	N 1|	N 2|	N 3|	N 4|	N 5
    P 1|	L  |	F  |	F  |	-  |	-  |	-
    P 2|	-  |	L  |	F  |	F  |	-  |	-
    P 3|	-  |	-  |	L  |	F  |	F  |	-
    P 4|	-  |	-  |	-  |	L  |	F  |	F
    P 5|	F  |	-  |	-  |	-  |	L  |	F
    P 6|	F  |	F  |	-  |	-  |	-  |	L

---
+++ chaos-experiments/scripts/disconnect-leaders-one-way.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -exuo pipefail
+
+source utils.sh
+
+## Intended for Production L cluster
+
+# TODO: add assertion for 6 partitions or at least 4
+
+partition=1
+namespace=$(getNamespace)
+gateway=$(getGateway)
+
+# determine leader for partition one
+index=$(getIndexOfPodForPartitionInState "$partition" "LEADER")
+leader=$(getBroker "$index")
+leaderIp=$(kubectl get pod "$leader" -n "$namespace" --template="{{.status.podIP}}")
+
+# determine leader for partition three
+index=$(getIndexOfPodForPartitionInState "4" "LEADER")
+leaderTwo=$(getBroker "$index")
+leaderTwoIp=$(kubectl get pod "$leaderTwo" -n "$namespace" --template="{{.status.podIP}}")
+
+
+# To print the topology in the journal
+retryUntilSuccess kubectl exec "$gateway" -n "$namespace" -- zbctl status --insecure
+
+# we put all into one function because we need to make sure that even after preemption the 
+# dependency is installed
+function disconnect() {
+ toChangedPod="$1"
+ targetIp="$2"
+
+ # update to have access to ip
+ kubectl exec -n "$namespace" "$toChangedPod" -- apt update
+ kubectl exec -n "$namespace" "$toChangedPod" -- apt install -y iproute2
+ kubectl exec "$toChangedPod" -n "$namespace" -- ip route add unreachable "$targetIp"
+
+}
+
+# We just disconnect in one way because otherwise it might lead to issues with an embedded gateway, which we currently have deployed in CC
+retryUntilSuccess disconnect "$leader" "$leaderTwoIp"
