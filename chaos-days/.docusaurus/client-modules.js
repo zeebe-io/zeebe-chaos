754a75e1807d2b10d6900a8df47479345238c92d
Christopher Zell <zelldon91@googlemail.com>
2021-10-04 08:58:57 +0200

    docs: ignore build folders

--- chaos-days/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/zell/goPath/src/github.com/zeebe-io/zeebe-chaos/chaos-days/node_modules/infima/dist/css/default/default.css"),
-  require("/home/zell/goPath/src/github.com/zeebe-io/zeebe-chaos/chaos-days/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/zell/goPath/src/github.com/zeebe-io/zeebe-chaos/chaos-days/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/home/zell/goPath/src/github.com/zeebe-io/zeebe-chaos/chaos-days/src/css/custom.css"),
-];
