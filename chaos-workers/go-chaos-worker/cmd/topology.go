d9cad69944a8c22c73fff91903807253330cfd15
Christopher Zell <zelldon91@googlemail.com>
2022-03-07 18:22:44 +0100

    refactor: extract port forwarding
    Create new helper methods to create gateway port forwarding and use helper in command

--- chaos-workers/go-chaos-worker/cmd/topology.go
+++ chaos-workers/go-chaos-worker/cmd/topology.go
@@ -1,17 +1,13 @@
 package cmd
 
 import (
-	"bytes"
 	"context"
 	"fmt"
-	"net/http"
 
 	"github.com/camunda-cloud/zeebe/clients/go/pkg/zbc"
 	"github.com/spf13/cobra"
 	"github.com/zeebe-io/zeebe-chaos/chaos-workers/go-chaos-worker/internal"
 	"google.golang.org/grpc"
-	"k8s.io/client-go/tools/portforward"
-	"k8s.io/client-go/transport/spdy"
 )
 
 func init() {
@@ -23,81 +19,27 @@ var topologyCmd = &cobra.Command{
 	Short: "Print the Zeebe topology deployed in the current namespace",
 	Long:  `Shows the current Zeebe topology, in the current kubernetes namespace.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		port := 26500
+		closeFn, err := internal.GatewayPortForward(port)
+		if err != nil {
+			panic(err.Error())
+		}
+		defer closeFn()
 
-		client := internal.CreateK8Client()
-		names, err := client.GetGatewayPodNames()
+		endpoint := fmt.Sprintf("localhost:%d", port)
+		client, err := zbc.NewClient(&zbc.ClientConfig{
+			GatewayAddress:         endpoint,
+			DialOpts:               []grpc.DialOption{},
+			UsePlaintextConnection: true,
+		})
+		if err != nil {
+			panic(err.Error())
+		}
+
+		response, err := client.NewTopologyCommand().Send(context.TODO())
 		if err != nil {
 			panic(err)
 		}
-
-		restClient := client.Clientset.CoreV1().RESTClient()
-
-		// based on terratest code
-		// https://github.com/gruntwork-io/terratest/blob/master/modules/k8s/tunnel.go#L187-L196
-		//and
-		// https://github.com/kubernetes/client-go/issues/51#issuecomment-436200428
-		postEndpoint := restClient.Post()
-		namespace := client.GetCurrentNamespace()
-		portForwardCreateURL := postEndpoint.
-			Resource("pods").
-			Namespace(namespace).
-			Name(names[0]).
-			SubResource("portforward").
-			URL()
-
-		// Construct the spdy client required by the client-go portforward library
-		config, err := client.ClientConfig.ClientConfig()
-		if err != nil {
-			panic(err)
-		}
-
-		transport, upgrader, err := spdy.RoundTripperFor(config)
-		if err != nil {
-			fmt.Printf("Error creating http client: %s\n", err)
-			panic(err)
-		}
-		dialer := spdy.NewDialer(upgrader, &http.Client{Transport: transport}, "POST", portForwardCreateURL)
-
-		// Construct a new PortForwarder struct that manages the instructed port forward tunnel
-		stopChan, readyChan := make(chan struct{}, 1), make(chan struct{}, 1)
-		out, errOut := new(bytes.Buffer), new(bytes.Buffer)
-		ports := []string{fmt.Sprintf("%d:%d", 26500, 26500)}
-		portforwarder, err := portforward.New(dialer, ports, stopChan, readyChan, out, errOut)
-		if err != nil {
-			fmt.Printf("Error creating port forwarding tunnel: %s", err)
-			panic(err)
-		}
-
-		// Open the tunnel in a goroutine so that it is available in the background. Report errors to the main goroutine via
-		// a new channel.
-		errChan := make(chan error)
-		go func() {
-			errChan <- portforwarder.ForwardPorts()
-		}()
-
-		// Wait for an error or the tunnel to be ready
-		select {
-		case err = <-errChan:
-			fmt.Printf("Error starting port forwarding tunnel: %s", err)
-			panic(err)
-		case <-portforwarder.Ready:
-			fmt.Printf("Successfully created port forwarding tunnel")
-
-			endpoint := fmt.Sprintf("localhost:%d", 26500)
-			client, err := zbc.NewClient(&zbc.ClientConfig{
-				GatewayAddress:         endpoint,
-				DialOpts:               []grpc.DialOption{},
-				UsePlaintextConnection: true,
-			})
-			if err != nil {
-				panic(err.Error())
-			}
-
-			response, err := client.NewTopologyCommand().Send(context.TODO())
-			if err != nil {
-				panic(err)
-			}
-			fmt.Printf("Response topology, %s", response.String())
-		}
+		fmt.Printf("Response topology, %s", response.String())
 	},
 }
--- chaos-workers/go-chaos-worker/internal/pods.go
+++ chaos-workers/go-chaos-worker/internal/pods.go
@@ -1,10 +1,16 @@
 package internal
 
 import (
+	"bytes"
 	"context"
+	"fmt"
+	"net/http"
+	"net/url"
 
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/portforward"
+	"k8s.io/client-go/transport/spdy"
 )
 
 func (c K8Client) GetBrokerPodNames() ([]string, error) {
@@ -42,4 +48,79 @@ func (c K8Client) GetGatewayPodNames() ([]string, error) {
 	}
 
 	return c.extractPodNames(list)
+}
+
+// GatewayPortForward creates a port forwarding to a zeebe gateway with the given port
+// https://github.com/gruntwork-io/terratest/blob/master/modules/k8s/tunnel.go#L187-L196
+// https://github.com/kubernetes/client-go/issues/51#issuecomment-436200428
+func GatewayPortForward(port int) (func(), error) {
+	client := CreateK8Client()
+	names, err := client.GetGatewayPodNames()
+	if err != nil {
+		return nil, err
+	}
+
+	portForwardCreateURL := createPortForwardUrl(client, names)
+	portForwarder, err := createPortForwarder(port, client, portForwardCreateURL)
+	if err != nil {
+		return nil, err
+	}
+
+	// Open the tunnel in a goroutine so that it is available in the background. Report errors to the main goroutine via
+	// a new channel.
+	errChan := make(chan error)
+	go func() {
+		errChan <- portForwarder.ForwardPorts()
+	}()
+
+	// Wait for an error or the tunnel to be ready
+	select {
+	case err = <-errChan:
+		fmt.Printf("Error starting port forwarding tunnel: %s", err)
+		return nil, err
+	case <-portForwarder.Ready:
+		fmt.Printf("Successfully created port forwarding tunnel")
+		return func() {
+			portForwarder.Close()
+		}, nil
+	}
+}
+
+// Create the k8 port forwarder, with the given port and k8 client
+func createPortForwarder(port int, client K8Client, portForwardCreateURL *url.URL) (*portforward.PortForwarder, error) {
+	// Construct the spdy client required by the client-go portforward library
+	config, err := client.ClientConfig.ClientConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	transport, upgrader, err := spdy.RoundTripperFor(config)
+	if err != nil {
+		fmt.Printf("Error creating http client: %s\n", err)
+		return nil, err
+	}
+	dialer := spdy.NewDialer(upgrader, &http.Client{Transport: transport}, "POST", portForwardCreateURL)
+
+	// Construct a new PortForwarder struct that manages the instructed port forward tunnel
+	stopChan, readyChan := make(chan struct{}, 1), make(chan struct{}, 1)
+	out, errOut := new(bytes.Buffer), new(bytes.Buffer)
+	ports := []string{fmt.Sprintf("%d:%d", port, 26500)}
+	portforwarder, err := portforward.New(dialer, ports, stopChan, readyChan, out, errOut)
+	if err != nil {
+		fmt.Printf("Error creating port forwarding tunnel: %s", err)
+		return nil, err
+	}
+	return portforwarder, nil
+}
+
+// createPortForwardUrl constructs the Url to which is used to create the port forwarding
+func createPortForwardUrl(client K8Client, names []string) *url.URL {
+	restClient := client.Clientset.CoreV1().RESTClient()
+	portForwardCreateURL := restClient.Post().
+		Resource("pods").
+		Namespace(client.GetCurrentNamespace()).
+		Name(names[0]).
+		SubResource("portforward").
+		URL()
+	return portForwardCreateURL
 }
