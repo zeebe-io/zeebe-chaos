db86e95f3ac8a50f9852310fb2fb503ab223eb89
Christopher Zell <zelldon91@googlemail.com>
2022-03-07 09:58:16 +0100

    style: format files

--- chaos-workers/go-chaos-worker/cmd/root.go
+++ chaos-workers/go-chaos-worker/cmd/root.go
@@ -1,1 +1,26 @@
 package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+  Use:   "zbchaos",
+  Short: "Zeebe chaos is a chaos experiment tool for Zeebe",
+  Long: `A chaos experimenting toolkit for Zeebe. 
+		 Ideally to inject some chaos into your brokers and gateways.`,
+  Run: func(cmd *cobra.Command, args []string) {
+    // Do Stuff Here
+	  fmt.Println("Zeebe Chaos")
+  },
+}
+
+func Execute() {
+  if err := rootCmd.Execute(); err != nil {
+    fmt.Fprintln(os.Stderr, err)
+    os.Exit(1)
+  }
+}
--- chaos-workers/go-chaos-worker/main.go
+++ chaos-workers/go-chaos-worker/main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"github.com/zeebe-io/zeebe-chaos/chaos-workers/go-chaos-worker/cmd"
+)
 
 func main() {
-	fmt.Println("hello world")
+	cmd.Execute()
 }
