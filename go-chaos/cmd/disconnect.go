0efd7452ca755063fefa23a7ff01a06afb97e6d2
Christopher Zell <zelldon91@googlemail.com>
2022-08-10 14:10:52 +0200

    feat: add net capability patch

---
+++ go-chaos/cmd/disconnect.go
@@ -0,0 +1,46 @@
+// Copyright 2022 Camunda Services GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/zeebe-io/zeebe-chaos/go-chaos/internal"
+)
+
+func init() {
+	rootCmd.AddCommand(disconnect)
+
+}
+
+var disconnect = &cobra.Command{
+	Use:   "disconnect",
+	Short: "Disconnect Zeebe brokers",
+	Long:  `Disconnect Zeebe brokers with a certain role and given partition.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		k8Client, err := internal.CreateK8Client()
+		if err != nil {
+			panic(err)
+		}
+
+		err = k8Client.ApplyNetworkPatch()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Patched statefulset")
+	},
+}
---
+++ go-chaos/internal/statefulset.go
@@ -0,0 +1,52 @@
+
+
+
+package internal
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+func (c K8Client) ApplyNetworkPatch() (error) {
+
+	// todo support cloud
+	listOptions := metav1.ListOptions{
+		LabelSelector: "app=camunda-platform",
+	}
+
+	statefulSetList, err := c.Clientset.AppsV1().StatefulSets(c.GetCurrentNamespace()).List(context.TODO(), listOptions)
+	if err != nil {
+		return err
+	}
+	
+	if len(statefulSetList.Items) <= 0 {
+		return errors.New(fmt.Sprintf("Expected to find the Zeebe statefulset but nothing was found in namespace %s", c.GetCurrentNamespace()))
+	}
+
+	statefulSet := statefulSetList.Items[0]
+
+	patch := []byte(`{
+		"spec":{
+			"template":{
+				"spec":{
+					"containers":[
+						{
+							"name": "zeebe",
+							"securityContext":{
+								"capabilities":{
+									"add":["NET_ADMIN"]
+								}
+							}
+						}]
+				}
+			}
+		}
+	}`)
+	_, err := c.Clientset.AppsV1().StatefulSets(c.GetCurrentNamespace()).Patch(context.TODO(), statefulSet.Name, types.StrategicMergePatchType, patch, metav1.PatchOptions{})
+	return err
+}
