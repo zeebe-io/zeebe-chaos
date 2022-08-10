5fba330a1704de000399d18b92fa8dc0bec44964
Christopher Zell <zelldon91@googlemail.com>
2022-08-10 11:48:56 +0200

    test: add test for resolve kubeconfig

--- go-chaos/internal/k8helper_test.go
+++ go-chaos/internal/k8helper_test.go
@@ -1,9 +1,11 @@
 package internal
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"k8s.io/client-go/util/homedir"
 )
 
 func Test_CreateK8ClientWithPath(t *testing.T) {
@@ -18,4 +20,15 @@ func Test_CreateK8ClientWithPath(t *testing.T) {
 	namespace, _, err := client.ClientConfig.Namespace()
 	assert.NoError(t, err)
 	assert.Equal(t, "kind-default", namespace)
+}
+
+func Test_ResolveDefaultKubePath(t *testing.T) {
+	// given
+	home := homedir.HomeDir()
+
+	// when
+	path := findKubeconfigPath()
+
+	// then
+	assert.Equal(t, strings.Join([]string{home, ".kube/config"}, "/"), *path)
 }
