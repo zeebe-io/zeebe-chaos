eb865be6467b9a1ee22fccbf2185b9a02c3878fa
Christopher Zell <zelldon91@googlemail.com>
2022-08-16 14:12:35 +0200

    style: gofmt

--- go-chaos/internal/labels.go
+++ go-chaos/internal/labels.go
@@ -16,8 +16,9 @@ func getSaasBrokerLabels() string {
 	// For backwards compatability the brokers kept the gateway labels, for a statefulset the labels are not modifiable
 	// To still be able to distinguish the standalone gateway with the broker, the gateway got a new label.
 	labelSelector := metav1.LabelSelector{
-		MatchLabels: map[string]string{"app.kubernetes.io/app" : "zeebe", "app.kubernetes.io/component": "gateway"},
+		MatchLabels: map[string]string{"app.kubernetes.io/app": "zeebe", "app.kubernetes.io/component": "gateway"},
 	}
+
 	return labels.Set(labelSelector.MatchLabels).String()
 }
 
@@ -32,7 +33,7 @@ func getSaasGatewayLabels() string {
 	// For backwards compatability the brokers kept the gateway labels, for a statefulset the labels are not modifiable
 	// To still be able to distinguish the standalone gateway with the broker, the gateway got a new label.
 	labelSelector := metav1.LabelSelector{
-		MatchLabels: map[string]string{"app.kubernetes.io/app" : "zeebe", "app.kubernetes.io/component": "standalone-gateway"},
+		MatchLabels: map[string]string{"app.kubernetes.io/app": "zeebe", "app.kubernetes.io/component": "standalone-gateway"},
 	}
 	return labels.Set(labelSelector.MatchLabels).String()
 }
