10aa47d15dd0b4326ba878aaf579e7f6591f353e
Ole Schönburg <ole.schoenburg@gmail.com>
2022-10-31 10:57:52 +0100

    feat: k8helper provides a dynamic client

--- go-chaos/internal/k8helper.go
+++ go-chaos/internal/k8helper.go
@@ -16,6 +16,7 @@ package internal
 
 import (
 	"fmt"
+	"k8s.io/client-go/dynamic"
 	"path/filepath"
 
 	"k8s.io/client-go/kubernetes"
@@ -29,8 +30,9 @@ import (
 )
 
 type K8Client struct {
-	ClientConfig clientcmd.ClientConfig
-	Clientset    kubernetes.Interface
+	ClientConfig  clientcmd.ClientConfig
+	DynamicClient dynamic.Interface
+	Clientset     kubernetes.Interface
 }
 
 // Returns the current namespace, defined in the kubeconfig
@@ -66,8 +68,11 @@ func createK8Client(settings KubernetesSettings) (K8Client, error) {
 	if Verbosity {
 		fmt.Printf("Connecting to %s\n", namespace)
 	}
-
-	return K8Client{Clientset: clientset, ClientConfig: clientConfig}, nil
+	dynamicClient, err := dynamic.NewForConfig(k8ClientConfig)
+	if err != nil {
+		return K8Client{}, err
+	}
+	return K8Client{Clientset: clientset, ClientConfig: clientConfig, DynamicClient: dynamicClient}, nil
 }
 
 type KubernetesSettings struct {
