fce92f47d1a238f5d1a5565e549d3ae9d4ac1c49
Christopher Zell <zelldon91@googlemail.com>
2022-11-15 21:44:13 +0100

    test: support dynamic client and crd
    In tests we are able to support CRD and the saas flag

--- go-chaos/internal/helper_test.go
+++ go-chaos/internal/helper_test.go
@@ -16,12 +16,17 @@ package internal
 
 import (
 	"context"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	v12 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	dynamicFake "k8s.io/client-go/dynamic/fake"
 	"k8s.io/client-go/kubernetes/fake"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -29,7 +34,13 @@ import (
 )
 
 func CreateFakeClient() K8Client {
-	k8Client := K8Client{Clientset: fake.NewSimpleClientset(), ClientConfig: &testClientConfig{namespace: "testNamespace"}}
+	scheme := runtime.NewScheme()
+	groupVersionKind := schema.GroupVersionKind{Group: "cloud.camunda.io", Version: "v1alpha1", Kind: "zeebeclusters"}
+	scheme.AddKnownTypeWithName(groupVersionKind, &unstructured.Unstructured{})
+
+	k8Client := K8Client{Clientset: fake.NewSimpleClientset(),
+		DynamicClient: dynamicFake.NewSimpleDynamicClient(scheme),
+		ClientConfig:  &testClientConfig{namespace: "testNamespace"}}
 	return k8Client
 }
 
@@ -76,6 +87,20 @@ func (c K8Client) CreateDeploymentWithLabelsAndName(t *testing.T, selector *meta
 	}, metav1.CreateOptions{})
 
 	require.NoError(t, err)
+}
+
+
+func (c K8Client) createSaaSCRD(t *testing.T) {
+	zeebeCrd := schema.GroupVersionResource{Group: "cloud.camunda.io", Version: "v1alpha1", Resource: "zeebeclusters"}
+	obj := &unstructured.Unstructured{}
+	namespace := c.GetCurrentNamespace()
+	clusterId := strings.TrimSuffix(namespace, "-zeebe")
+	obj.SetName(clusterId)
+
+	_, err := c.DynamicClient.Resource(zeebeCrd).Create(context.TODO(), obj, metav1.CreateOptions{})
+	require.NoError(t, err)
+
+	saasEnv = c.isSaaSEnvironment()
 }
 
 func (c K8Client) CreateStatefulSetWithLabelsAndName(t *testing.T, selector *metav1.LabelSelector, name string) {
