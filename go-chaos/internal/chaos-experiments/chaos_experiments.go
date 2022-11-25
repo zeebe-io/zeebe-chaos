042122d67287c4202588f80603a6238735dc2d19
Christopher Zell <zelldon91@googlemail.com>
2022-11-25 09:36:43 +0100

    refactor: return experiments struct

--- go-chaos/internal/chaos-experiments/chaos_experiments.go
+++ go-chaos/internal/chaos-experiments/chaos_experiments.go
@@ -14,30 +14,33 @@ var chaosContent embed.FS
 
 const experimentFileName = "experiment.json"
 
-func readExperimentsForClusterPlan(clusterPlan string) (string, error) {
+type Experiments struct {
+	Experiments []map[string]interface{}
+}
+
+func ReadExperimentsForClusterPlan(clusterPlan string) (Experiments, error) {
 	normalizedClusterPlan := strings.ToLower(strings.Replace(clusterPlan, " ", "", -1))
 	rootPath := fmt.Sprintf("camunda-cloud/%s", normalizedClusterPlan)
 	dirEntries, err := chaosContent.ReadDir(rootPath)
 	if err != nil {
-		return "", err
+		return Experiments{}, err
 	}
 
-	var experiments []map[string]interface{}
+	experiments := Experiments{}
 	for _, dir := range dirEntries {
 		if dir.IsDir() {
 			experimentBytes, err := chaosContent.ReadFile(fmt.Sprintf("%s/%s/%s", rootPath, dir.Name(), experimentFileName))
 			if err != nil {
-				return "", err
+				return experiments, err
 			}
 			var jsonObj map[string]interface{}
 			err = json.Unmarshal(experimentBytes, &jsonObj)
 			if err != nil {
-				return "", err
+				return experiments, err
 			}
-			experiments = append(experiments, jsonObj)
+			experiments.Experiments = append(experiments.Experiments, jsonObj)
 		}
 	}
 
-	jsonBytes, err := json.Marshal(experiments)
-	return string(jsonBytes), err
+	return experiments, err
 }
--- go-chaos/internal/chaos-experiments/chaos_experiments_test.go
+++ go-chaos/internal/chaos-experiments/chaos_experiments_test.go
@@ -1,32 +1,24 @@
 package chaos_experiments
 
 import (
-	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-type Experiment struct {
-	Version string
-	Title   string
-}
-
 func Test_ShouldReadExperiments(t *testing.T) {
 	// given
 
 	// when
-	experimentsForClusterPlan, err := readExperimentsForClusterPlan("Production - S")
+	experimentsForClusterPlan, err := ReadExperimentsForClusterPlan("Production - S")
 
 	// then
 	require.NoError(t, err)
 	assert.NotNil(t, experimentsForClusterPlan)
 
-	var experiments []Experiment
-	err = json.Unmarshal([]byte(experimentsForClusterPlan), &experiments)
 	require.NoError(t, err)
-	assert.Greater(t, len(experiments), 2)
-	assert.NotEqual(t, experiments[0], experiments[1])
-	assert.NotEqual(t, "Zeebe deployment distribution", experiments[0])
+	assert.Greater(t, len(experimentsForClusterPlan.Experiments), 2)
+	assert.NotEqual(t, experimentsForClusterPlan.Experiments[0], experimentsForClusterPlan.Experiments[1])
+	assert.NotEqual(t, "Zeebe deployment distribution", experimentsForClusterPlan.Experiments[0])
 }
