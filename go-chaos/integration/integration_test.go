bdc8daec7e9a4e05a35fd85372202099c50eed44
Christopher Zell <zelldon91@googlemail.com>
2022-12-05 15:43:40 +0100

    test: new integration test
    Add new integration tests which verifies the complete cycle of deploying models, creating instance and running workers

--- go-chaos/integration/integration_test.go
+++ go-chaos/integration/integration_test.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
+	"github.com/zeebe-io/zeebe-chaos/go-chaos/cmd"
 	"github.com/zeebe-io/zeebe-chaos/go-chaos/internal"
 
 	"github.com/testcontainers/testcontainers-go"
@@ -27,6 +29,47 @@ func Test_ShouldBeAbleToDeployChaosModels(t *testing.T) {
 
 	// then
 	require.NoError(t, err)
+}
+
+func Test_ShouldBeAbleToRunExperiments(t *testing.T) {
+	// given
+	internal.Verbosity = true
+	ctx := context.Background()
+	container := CreateEZEContainer(t, ctx)
+	defer container.StopLogProducer()
+	mappedPort, err := container.MappedPort(ctx, "26500/tcp")
+	require.NoError(t, err)
+	zeebeClient, err := internal.CreateZeebeClient(mappedPort.Int())
+	require.NoError(t, err)
+	err = internal.DeployChaosModels(zeebeClient)
+
+	// required variables
+	vars := make(map[string]interface{})
+	vars["clusterPlan"] = "test" // specifies the cluster plan for which we read the experiments
+	vars["clusterId"] = ""       // need to be set to empty string, otherwise we run into a SIGSEG
+
+	commandStep3, err := zeebeClient.NewCreateInstanceCommand().BPMNProcessId("chaosToolkit").LatestVersion().VariablesFromMap(vars)
+	require.NoError(t, err)
+	timeout := time.After(60 * time.Second)
+	done := make(chan struct{})
+
+	// when
+	// concurrent open workers and start instance (await result)
+	go cmd.OpenWorkers(zeebeClient)
+	go func() {
+		internal.LogInfo("Create ChaosToolkit instance")
+		response, err := commandStep3.WithResult().Send(ctx)
+		require.NoError(t, err)
+		internal.LogInfo("Instance %d [definition %d ] completed", response.ProcessInstanceKey, response.ProcessDefinitionKey)
+		close(done)
+	}()
+
+	// then
+	select {
+	case <-timeout:
+		t.Fatal("Process instance hasn't been completed in time.")
+	case <-done: // wait until instance has been completed
+	}
 }
 
 func CreateEZEContainer(t *testing.T, ctx context.Context) testcontainers.Container {
